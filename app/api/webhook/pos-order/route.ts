b1c1618bd92bc79896dc278dbd81b50168e19976
Arnold Bopeto <antostif07@gmail.com>
2025-10-07 01:59:18 +0100

    add new observ for cwebhook

--- app/api/webhook/pos-order/route.ts
+++ app/api/webhook/pos-order/route.ts
@@ -1,10 +1,30 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+// Cache temporaire en mémoire
+const processedOrders = new Set<string>();
+
 export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
 
         console.log(body);
+
+        if (body._model !== "pos.order" || !body.data) {
+            return NextResponse.json({ message: "Not a POS event" }, { status: 200 });
+        }
+
+        const order = body.data;
+
+        const orderId = order.id || order.name;
+
+        // 1️⃣ Vérifie si cette commande a déjà été traitée
+        if (processedOrders.has(orderId)) {
+            console.log(`Commande ${orderId} déjà traitée.`);
+            return NextResponse.json({ success: false, message: "Already processed" });
+        }
+
+        // 2️⃣ Ajoute dans le cache
+        processedOrders.add(orderId);
 
         return NextResponse.json({success: true,});
     } catch (error: unknown) {
