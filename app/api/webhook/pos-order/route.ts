22fa49ce240a04e8e4310a328ae72c0e2e08d925
Arnold Bopeto <antostif07@gmail.com>
2025-10-07 12:43:31 +0100

    add template payload

--- app/api/webhook/pos-order/route.ts
+++ app/api/webhook/pos-order/route.ts
@@ -1,32 +1,73 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-// Cache temporaire en mémoire
-const processedOrders = new Set<string>();
-
 export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
 
         console.log(body);
-        console.log(processedOrders);
+        const {amount_paid, partner_id} = body
+
+        const payload = {
+            messaging_product: "whatsapp",
+            recipient_type: "individual",
+            to: "+243985976862",
+            type: "template",
+            template: {
+                "name": "promo_deux_achat_un_ajout",
+                "language": {
+                    "code": "fr"
+                },
+                "components": [
+                    {
+                        type: "header",
+                        parameters: [
+                            {
+                                type: "text",
+                                parameter_name: "nom_de_la_promo",
+                                text: "Facture test"
+                            }
+                        ]
+                    },
+                    {
+                        type: "body",
+                        parameters: [
+                            {
+                                "type": "text",
+                                "parameter_name": "date_debut",
+                                "text": partner_id,
+                            },
+                            {
+                                "type": "text",
+                                "parameter_name": "prix_de_depart",
+                                "text": `${amount_paid} $`
+                            },
+                            {
+                                "type": "text",
+                                "parameter_name": "date_fin",
+                                "text": partner_id,
+                            },
+                        ]
+                    }
+                ]
+            }
+        }
         
 
         if (body._model !== "pos.order" || !body.data) {
             return NextResponse.json({ message: "Not a POS event" }, { status: 200 });
         }
 
-        const order = body.data;
+        await fetch(`https://graph.facebook.com/v21.0/${process.env.WHATSAPP_PHONE_ID}/messages`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${process.env.WHATSAPP_TOKEN}`,
+            },
+            body: JSON.stringify(payload),
+        });
+        // const order = body.data;
 
-        const orderId = order.id || order.name;
-
-        // 1️⃣ Vérifie si cette commande a déjà été traitée
-        if (processedOrders.has(orderId)) {
-            console.log(`Commande ${orderId} déjà traitée.`);
-            return NextResponse.json({ success: false, message: "Already processed" });
-        }
-
-        // 2️⃣ Ajoute dans le cache
-        processedOrders.add(orderId);
+        // const orderId = order.id || order.name;
 
         return NextResponse.json({success: true,});
     } catch (error: unknown) {
